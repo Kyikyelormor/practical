85567b9bbfee51e989f34fcb7fe20a926b616632
Kyikyelormor <122827422+Kyikyelormor@users.noreply.github.com>
2024-03-03 10:11:04 +0100

    practical

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Write a JavaScript function to convert an amount into coins.
+// Write a function to find the area of a given rectangle (try with params).
+// Write a function called `transmogrifier`. This function should accept three arguments,
+// which you can assume will be numbers. Your function should return the "transmogrified" result.
+// The transmogrified result of three numbers is the product of the first two numbers,
+//  raised to the power of the third number.
+
+
+
+    function convert (amount, currency){
+    let coins = Math.floor(amount/currency)
+    let width = amount %currency
+     if ()
+    return convert
+}
+   
